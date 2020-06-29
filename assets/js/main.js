b48feed34f8548e0d6246fb753ce960f611f4a53
serena-chen <sacdesign1@gmail.com>
2020-06-29 14:14:03 -0400

    font size change

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,12 +95,14 @@ $(document).ready(function() {
 
   $('.nav-link--about').click(function() {
       $('.header-content__container').addClass('hideme');
+      $('.main-content__container').addClass('hideme');
       $('.popup__container--about').addClass('showme');
   });
 
   $('.nav-link--close').click(function() {
       $('.popup__container--about').removeClass('showme');
       $('.header-content__container').removeClass('hideme');
+      $('.main-content__container').removeClass('hideme');
   });
 
 
