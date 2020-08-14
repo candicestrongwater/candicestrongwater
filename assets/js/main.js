5875a2860bcbbc9c358100bf7b16037e31999a9f
serena-chen <sacdesign1@gmail.com>
2020-08-13 23:22:28 -0400

    edits r5
    Final text. Add custom domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ connectedCallback() {
       </div>
 
       <div class="flex-item--right header-item--right">
-        <a class="nav-link nav-link--exhibitions" href="./exhibitions.html">Exhibitions & Programs</a>
+        <a class="nav-link nav-link--exhibitions" href="./index.html">Exhibitions & Programs</a>
         <a class="nav-link nav-link--programs" href="./writing.html">Writing</a>
         <span class="nav-link nav-link--about">About</span>
       </div>
@@ -26,20 +26,18 @@ connectedCallback() {
 
     <div class="popup__container--about">
       <div class="flex-item--left about-item--left">
-         <span class="about-text">
-         I am an independent curator and cultural producer based in New York. The work included here is a selection of projects spanning exhibitions, programs, oral histories, artist publications, radio broadcasts, and site-specific installations.
-         <br><br>
-         I hold a BA in Film Theory & Criticism from Muhlenberg College and am a MA candidate at the Center for Curatorial Studies at Bard College (2021). My current research traces a media history of educational technologies, focusing on the classroom setting in the U.S. from the 1950's to today.
-         <br><br>
-         I am also the Reserve Film and Video Collection Fellow (2020–21) at the New York Public Library, focusing on the history of the Donnell Media Center (1955–2008).
+         <span class="about-text-lc">
+         I am an independent curator and cultural producer. The work included here is a selection of projects spanning exhibitions, programs, oral histories, artist publications, radio broadcasts, and site-specific installations. I hold a BA in Film Theory & Criticism at Muhlenberg College and am pursuing an MA at the Center for Curatorial Studies at Bard College (2021). My current research traces a media history of educational technologies, focusing on the classroom setting in the US from the 1950s to today. I am the Reserve Film and Video Collection Fellow (2020–21) at the New York Public Library for the Performing Arts, conducting research on the history of the Donnell Media Center.
          <br><br>
          Website accessibility: This site is in the process of featuring image descriptions.
-         <br><br>
          </span>
 
-         <a class="nav-link" href="mailto:strongwater.candice@gmail.com" target="_blank">EMAIL</a>
+         <br><br>
+
+         <a class="nav-link" href="mailto:strongwater.candice@gmail.com" target="_blank">EMAIL</a><span class="about-text-lc"> *Feel free to message me</span>
          <br>
          <a class="nav-link" href="https://www.instagram.com/cancode/?hl=en" target="_blank">INSTAGRAM</a>
+
       </div>
 
       <div class="flex-item--right about-item--right">
