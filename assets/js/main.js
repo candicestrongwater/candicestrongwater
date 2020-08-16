97b496f547c34958c9654f3300e49a40998f61b4
serena-chen <sacdesign1@gmail.com>
2020-08-16 13:22:07 -0400

    popup fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,12 +62,15 @@ $('.nav-link--about').click(function() {
     $('.header-content__container').addClass('hideme');
     $('.main-content__container').addClass('hideme');
     $('.popup__container--about').addClass('showme');
+    $('body').addClass('bg-color-change');
+
 });
 
 $('.nav-link--close').click(function() {
     $('.popup__container--about').removeClass('showme');
     $('.header-content__container').removeClass('hideme');
     $('.main-content__container').removeClass('hideme');
+    $('body').removeClass('bg-color-change');
 });
 
 
